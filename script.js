3e373dc4107e5facf72afbb19fba18c61cd08276
Tether <tarun.sankhla.prc@gmail.com>
2022-01-30 02:57:22 +0530

    documentation style and componenet updates

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var nav_hider = document.querySelector('.hide-nav');
 var nav_toggler = document.querySelector('.navbar-togglers');
 var nav_closer = document.querySelector('.navbar-close-toggle');
 
+var btn_scrolltop=document.querySelector('#scroll-top-btn');
 console.log(document.body.clientWidth);
 
 
@@ -14,7 +15,7 @@ jQuery(document).ready(function($) {
         if(document.body.clientWidth <= 800){
             console.log("a",document.body.clientWidth);
             drawer.classList.add('hide-nav');
-            
+            nav_closer.style.display ="block";
             document.querySelector('.hide-nav').style.visibility ="hidden";
             
             document.querySelector('.container-drawer').style.visibility ="visible";
@@ -27,7 +28,7 @@ jQuery(document).ready(function($) {
         }
         else{
             console.log("b",document.body.clientWidth);
-            nav_closer.style.visibility ="hidden";
+            nav_closer.style.display ="none";
             document.querySelector('.container-drawer').style.visibility ="hidden";
             if(document.querySelector('.hide-nav')){
             document.querySelector('.hide-nav').style.visibility ="visible";
@@ -51,4 +52,22 @@ nav_toggler.addEventListener('click',()=>{
 nav_closer.addEventListener('click',()=>{
     console.log("tog");
     document.querySelector('.hide-nav').style.visibility ="hidden";
+})
+
+
+// scroll to top
+window.onscroll = function(){ 
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop >20){
+        btn_scrolltop.style.display ="block";
+        console.log("Scroller btn");
+    }else{
+        btn_scrolltop.style.display ="none";
+        console.log("Scroller btn");
+    }
+}
+
+btn_scrolltop.addEventListener('click',()=>{
+    console.log("Scroller click");
+    document.body.scrollTop =0;
+    document.documentElement.scrollTop =0;
 })
